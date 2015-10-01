eab1b61acb6758a803117fd8e1bf02a6d67200e0
nao20010128nao <nao20010128@gmail.com>
2015-10-01 19:20:19 +0900

    削除

--- src/com/nao20010128nao/MCPE/SC/Default.java
+++ src/com/nao20010128nao/MCPE/SC/Default.java
@@ -29,69 +29,6 @@ public class Default extends SmartFindViewActivity
 		whileCreate=false;
 		setInside(((Intent)getIntent().clone()).setClass(this,MainActivity.class));
 	}
-/*
-	@Override
-	public void setContentView(int layoutResID) {
-		// TODO: Implement this method
-		if(whileCreate)super.setContentView(layoutResID);
-		View v=getLayoutInflater().inflate(layoutResID,null,false);
-		setContentView(v);
-	}
-
-	@Override
-	public void setContentView(View view) {
-		// TODO: Implement this method
-		if(whileCreate)super.setContentView(view);
-		content.removeAllViews();
-		content.addView(view);
-		view.setLayoutParams(new ViewGroup.LayoutParams(-1,-1));
-		runIndexing();
-	}
-
-	@Override
-	public View findViewById(int id) {
-		// TODO: Implement this method
-		if(whileCreate)return super.findViewById(id);
-		View s=super.findViewById(id);
-		View c=content.findViewById(id);
-		View i=index.get(id);
-		if(s==null){
-			if(c==null){
-				if(i==null){
-					return null;
-				}else{
-					return i;
-				}
-			}else{
-				return c;
-			}
-		}else{
-			return s;
-		}
-	}
-	private void runIndexing(){
-		for(int i=0;i<content.getChildCount();i++){
-			View v=content.getChildAt(i);
-			if(v.getId()!=View.NO_ID){
-				index.put(v.getId(),v);
-			}
-			if(v instanceof ViewGroup){
-				findAll((ViewGroup)v);
-			}
-		}
-	}
-	private void findAll(ViewGroup vg){
-		for(int i=0;i<vg.getChildCount();i++){
-			View v=vg.getChildAt(i);
-			if(v.getId()!=View.NO_ID){
-				index.put(v.getId(),v);
-			}
-			if(v instanceof ViewGroup){
-				findAll((ViewGroup)v);
-			}
-		}
-	}
-	*/
 	public void setInside(Intent intent){
 		getLocalActivityManager().destroyActivity("main",true);
 		content.removeAllViews();
