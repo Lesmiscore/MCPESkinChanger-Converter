ff6758f0f7b7bef4b62183f8acf86cbdba547d0b
nao20010128nao <nao20010128@gmail.com>
2015-10-01 20:29:44 +0900

    いろいろ

--- src/com/nao20010128nao/MCPE/SC/InputSelected.java
+++ src/com/nao20010128nao/MCPE/SC/InputSelected.java
@@ -1,6 +1,7 @@
 package com.nao20010128nao.MCPE.SC;
 import com.nao20010128nao.MCPE.SC.misc.*;
 import android.os.*;
+import com.nao20010128nao.MC_PE.SkinChanger.CONVERTER.*;
 
 public class InputSelected extends SmartFindViewActivity
 {
@@ -8,5 +9,14 @@ public class InputSelected extends SmartFindViewActivity
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO: Implement this method
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.inputcheck);
+		new AsyncTask<String,Void,Formats>(){
+			public Formats doInBackground(String[]s){
+				String from=s[0];
+				Formats result=null;
+				
+				return result;
+			}
+		}.execute(getIntent().getDataString());
 	}
 }
