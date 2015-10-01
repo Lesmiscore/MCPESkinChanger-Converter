6bd5d2efa3b1d2fc9f8ca8a7ae7e1526e0793a84
nao20010128nao <nao20010128@gmail.com>
2015-10-01 20:05:25 +0900

    修正

--- src/com/nao20010128nao/MCPE/SC/InputSelected.java
+++ src/com/nao20010128nao/MCPE/SC/InputSelected.java
@@ -2,7 +2,7 @@ package com.nao20010128nao.MCPE.SC;
 import com.nao20010128nao.MCPE.SC.misc.*;
 import android.os.*;
 
-public class InputSelected extends Default
+public class InputSelected extends SmartFindViewActivity
 {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
