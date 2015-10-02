d94bbf08f822da8b58e71981aa1b3363532580a7
nao20010128nao <nao20010128@gmail.com>
2015-10-02 22:51:23 +0900

    テンポラリファイル

--- src/com/nao20010128nao/MCPE/SC/RunOnceApplication.java
+++ src/com/nao20010128nao/MCPE/SC/RunOnceApplication.java
@@ -1,14 +1,18 @@
 package com.nao20010128nao.MCPE.SC;
 import android.app.*;
+import java.io.*;
 
 public class RunOnceApplication extends Application {
 	boolean isCheckedMCPE=false;
+	public static File tmpFileA,tmpFileB;
 	public static RunOnceApplication instance;
 	@Override
 	public void onCreate() {
 		// TODO: Implement this method
 		super.onCreate();
 		instance = this;
+		tmpFileA=new File(getCacheDir(),"a");
+		tmpFileB=new File(getCacheDir(),"b");
 	}
 	public boolean isCheckedMCPE() {
 		return isCheckedMCPE;
